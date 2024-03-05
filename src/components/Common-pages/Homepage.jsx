13c70508920a0191e3b9ba37f02fa77ac54dce1d
thisisnidhinpk <thisisnidhinpk>
2024-03-05 07:40:54 +0530

    astro slider added

---
+++ src/components/Common-pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Mynenu from "./Mynenu";
+import Slider from "./Slider";
+
+function Homepage() {
+  return (
+    <div>
+      <Mynenu />
+      <Slider />
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ src/components/Common-pages/Mynenu.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Mynenu() {
+  return (
+    <div>
+        
+          {/* Your menu content goes here */}
+      <nav>
+        <ul>
+          <li>Home</li>
+          {/* Add more menu items as needed */}
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Mynenu
